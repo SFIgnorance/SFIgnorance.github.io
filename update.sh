42ce2215038e3c49a110e2a3ac534987c31315d2
SiddhantDeshmukh <sd486@exeter.ac.uk>
2020-08-16 19:25:03 +0200

    Layout photon

--- update.sh
+++ update.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
 # Update web page
+echo "Updating page..."
 cd scripts
 python setup_page.py
 cd ..
+echo "Page updated"
 
 # Commit to GitHub
+echo "Pushing to GitHub..."
 git add .
 git commit -m "$1"
 git push
